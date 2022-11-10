d5a94b2b72128c1cfd8aa6590bf1f8d340a6798b
SulyyKhan <suly.khan@hippodigital.co.uk>
2022-11-10 21:01:02 +0000

    v14 Prototype - Check Cohort
    - Flow done through to check your cohort
    
    - bulk of the work in the demographics page
    
    - added logic to return user back to check answers page after changing an input
    
    - additional logic added to capture answers or only specific checkboxes e.g. all checkbox

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,33 @@
 // ES6 or Vanilla JavaScript
+
+// Delete table row (used on check-condition-group page)
+
+function DeleteRow() {
+    // event.target will be the input element.
+    var td = event.target.parentNode; 
+    var tr = td.parentNode; // the row to be removed
+    tr.parentNode.removeChild(tr);
+}
+
+// uncheck radio when checkbox is selected
+
+function deSelectRadio() {
+    document.querySelector('input[name="sex-at-birth"]:checked').checked = false;
+}
+
+// uncheck radio when checkbox is selected
+
+function deSelectCheckbox() {
+    document.querySelector('input[name="not-applicable-sex"]:checked').checked = false;
+}
+
+// Check every ethnicity checkbox when all is selected
+
+function toggle(source) {
+    var checkboxes = document.querySelectorAll(
+        'input[name="ethnicity"], input[name="ethnicity-asian-1"], input[name="ethnicity-asian-2"], input[name="ethnicity-asian-3"], input[name="ethnicity-asian-4"], input[name="ethnicity-asian-5"], input[name="ethnicity-black-1"],input[name="ethnicity-black-2"], input[name="ethnicity-black-3"], input[name="ethnicity-mixed-1"], input[name="ethnicity-mixed-2"], input[name="ethnicity-mixed-3"], input[name="ethnicity-mixed-4"], input[name="ethnicity-white-1"], input[name="ethnicity-white-2"], input[name="ethnicity-white-3"], input[name="ethnicity-white-4"], input[name="ethnicity-white-5"], input[name="ethnicity-other-1"], input[name="ethnicity-other-2"] ');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != source)
+            checkboxes[i].checked = source.checked;
+    }
+}
--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,56 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// Contextual Routing for Check your cohort
+// What the route does:
+
+// When a user goes through a entire form, and reaches the check your cohort screen
+// we want them to have the ability to change and edit whawtever they have done
+// what we don't want is taking them back to a previous screen to change details
+// amd then having them go through the whole form again
+
+// So the contextual routes picks up on whether or not the user has reached the
+// check-cohort screen, if the flag on that screen is true, they'll be re-directed
+// straight back to the check-cohort screen after making changes
+// if not, they'll carry on with the form journey until they reach check-cohort
+
+// Contextual routing - add-cohort-details.html
+
+router.post("/cohort-builder/v14/add-cohort-details-answer", function (req, res) {
+    // grab value from the data store
+    let complete = req.session.data.complete
+    // if the journey is complete send back to the 'check-your-cohort' page
+    console.log(req.session.data)
+    if (complete === 'true') {
+      res.redirect('/cohort-builder/v14/check-cohort')  
+    } else {  
+      res.redirect('/cohort-builder/v14/add-demographic-rules')  
+    }
+})
+
+// Contextual routing - add-demographic-rules.html
+
+router.post("/cohort-builder/v14/add-demographic-rules-answer", function (req, res) {
+    // grab value from the data store
+    let complete = req.session.data.complete
+    // if the journey is complete send back to the 'check-your-cohort' page
+    console.log(req.session.data)
+    if (complete === 'true') {
+      res.redirect('/cohort-builder/v14/check-cohort')  
+    } else {  
+      res.redirect('/cohort-builder/v14/add-condition-group')  
+    }
+})
+
+// Set journey as complete - check-cohort.html
+
+router.get("/cohort-builder/v14/check-cohort", function (req, res) {  
+    // set data store variable
+    console.log("BEFORE", req.session.data)
+    req.session.data.complete = 'true'
+    console.log("AFTER", req.session.data)
+    // render the page
+    return res.render('cohort-builder/v14/check-cohort')
+})
+
 module.exports = router;
