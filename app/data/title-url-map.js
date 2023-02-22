abd4220ab965b5dc23b93198e4c3f8c200b4aeec
Suly <suly.khan@hippodigital.co.uk>
2023-02-22 15:06:04 +0000

    fix to title url map and content list aria label

--- app/data/title-url-map.js
+++ app/data/title-url-map.js
@@ -1,8 +1,9 @@
 const titleUrlMap = {
   "cohort-explorer/landing": "Cohorting as a Service (CaaS)",
   "cohort-explorer/cohort-library": "Cohort library",
+  "cohort-explorer/influenza-winter-22-23/influenza-cohort": "Influenza vaccinations 2022 to 2023: Cohort identification methodology",
 };
 
 module.exports = (url) => {
-  return titleUrlMap[Object.keys(titleUrlMap).find((key) => url.endsWith(key))];
+  return titleUrlMap[Object.keys(titleUrlMap).find((url) => url.endsWith(url))];
 }
