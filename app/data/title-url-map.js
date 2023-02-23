0d93abcb36ea4ed6ab45ef97cd53eb66e37fa451
Suly <suly.khan@hippodigital.co.uk>
2023-02-23 11:39:57 +0000

    Clinically coded eligibility + basic go back link restored

--- app/data/title-url-map.js
+++ app/data/title-url-map.js
@@ -1,9 +1,95 @@
+// Add page H1 titles and their file path below, nothing really else needs to be touched,
+// the logic will return whether or not a title exists for a page or not, if it does it will
+// return a title for the page in the go back link dynamically
+
+// locals.js also includes logic pulling from the map below
+
 const titleUrlMap = {
-  "cohort-explorer/landing": "Cohorting as a Service (CaaS)",
-  "cohort-explorer/cohort-library": "Cohort library",
-  "cohort-explorer/influenza-winter-22-23/influenza-cohort": "Influenza vaccinations 2022 to 2023: Cohort identification methodology",
+
+  // Top-level pages
+
+  "cohort-explorer/landing": {
+    title:"Cohorting as a Service (CaaS)",
+    parentPath: ""
+  },
+  "cohort-explorer/cohort-library": {
+    title:"Cohort library",
+    parentPath: "cohort-explorer/landing"
+  },
+  "cohort-explorer/condition-group-library": {
+    title:"Condition group library",
+    parentPath: "cohort-explorer/landing"
+  },
+  "cohort-explorer/data-integration-library": {
+    title:"Data integration library",
+    parentPath: "cohort-explorer/landing"
+  },
+  "cohort-explorer/support": {
+    title:"Support",
+    parentPath: "cohort-explorer/landing"
+  },
+
+  // Influenza Cohort
+
+  "cohort-explorer/influenza-winter-22-23/influenza-cohort": {
+    title:"Influenza vaccinations 2022 to 2023: Cohort identification methodology",
+    parentPath: "cohort-explorer/cohort-library"
+  },
+
+  // Influenza Cohort Condition Groups - Chronic Heart Disease
+
+  "cohort-explorer/influenza-winter-22-23/condition-groups/chronic-heart-disease": {
+    title:"Chronic heart disease",
+    parentPath: "cohort-explorer/influenza-winter-22-23/influenza-cohort"
+  },
+  
+  // Influenza Cohort Condition Groups - Chronic Kidney Disease
+
+  "cohort-explorer/influenza-winter-22-23/condition-groups/chronic-kidney-disease": {
+    title:"Chronic kidney disease",
+    parentPath: "cohort-explorer/influenza-winter-22-23/influenza-cohort"
+  },
+
+  // Influenza Cohort Condition Groups - Chronic Liver Disease
+
+  "cohort-explorer/influenza-winter-22-23/condition-groups/chronic-liver-disease": {
+    title:"Chronic liver disease",
+    parentPath: "cohort-explorer/influenza-winter-22-23/influenza-cohort"
+  },
+
+  // Influenza Cohort Condition Groups - Chronic Neurological Disease
+
+  "cohort-explorer/influenza-winter-22-23/condition-groups/chronic-neurological-disease": {
+    title:"Chronic neurological disease",
+    parentPath: "cohort-explorer/influenza-winter-22-23/influenza-cohort"
+  },
+
+  // Influenza Cohort Condition Groups - Chronic Respiratory Disease
+
+  "cohort-explorer/influenza-winter-22-23/condition-groups/chronic-respiratory-disease": {
+    title:"Chronic respiratory disease",
+    parentPath: "cohort-explorer/influenza-winter-22-23/influenza-cohort"
+  },
+  
+  // Influenza Cohort Condition Groups - Clinically Coded Eligibility
+
+  "cohort-explorer/influenza-winter-22-23/condition-groups/clinically-coded-eligibility": {
+    title:"Clinically coded eligibility",
+    parentPath: "cohort-explorer/influenza-winter-22-23/influenza-cohort"
+  },
 };
 
 module.exports = (url) => {
-  return titleUrlMap[Object.keys(titleUrlMap).find((url) => url.endsWith(url))];
+  if (!url) {
+    return
+  }
+  const match = titleUrlMap[Object.keys(titleUrlMap).find((key) => url.endsWith(key))];
+  if (!match) {
+    return
+  }
+  const parentTitle = titleUrlMap[match.parentPath] ? titleUrlMap[match.parentPath].title : "";
+  return {
+    title: match.title,
+    parentTitle
+  };
 }
--- app/locals.js
+++ app/locals.js
@@ -2,8 +2,13 @@ const getTitleFromUrl = require('./data/title-url-map.js');
 
 module.exports = (config) => (req, res, next) => {
   res.locals.serviceName = config.serviceName;
-  res.locals.previousPageTitle = getTitleFromUrl(req.headers.referer);
-  res.locals.currentPageTitle = getTitleFromUrl(req.url);
+  
+  const match = getTitleFromUrl(req.url)
+
+  if (match) {
+    res.locals.currentPageTitle = match.title
+    res.locals.previousPageTitle = match.parentTitle
+  }
 
   next();
 };
