4dd8987340abea8d932c47871bf3134c2bfa85c8
Suly <suly.khan@hippodigital.co.uk>
2023-10-03 12:24:20 +0100

    Updated v6 prototype and fixed v14 legacy prototype

--- app/routes.js
+++ app/routes.js
@@ -20,70 +20,72 @@ const router = express.Router();
 
 // Contextual routing - add-cohort-details.html
 
-router.post("/cohort-builder/v14/add-cohort-details-answer", function (req, res) {
+router.post("/build-cohort/v14/add-cohort-details-answer", function (req, res) {
     // grab value from the data store
     let complete = req.session.data.complete
     // if the journey is complete send back to the 'check-your-cohort' page
     console.log(req.session.data)
     if (complete === 'true') {
-      res.redirect('/cohort-builder/v14/check-cohort')  
+      res.redirect('/build-cohort/v14/check-cohort')  
     } else {  
-      res.redirect('/cohort-builder/v14/add-demographic-rules')  
+      res.redirect('/build-cohort/v14/add-demographic-rules')  
     }
 })
 
 // Contextual routing - add-demographic-rules.html
 
-router.post("/cohort-builder/v14/add-demographic-rules-answer", function (req, res) {
+router.post("/build-cohort/v14/add-demographic-rules-answer", function (req, res) {
     // grab value from the data store
     let complete = req.session.data.complete
     // if the journey is complete send back to the 'check-your-cohort' page
     console.log(req.session.data)
     if (complete === 'true') {
-      res.redirect('/cohort-builder/v14/check-cohort')  
+      res.redirect('/build-cohort/v14/check-cohort')  
     } else {  
-      res.redirect('/cohort-builder/v14/add-condition-group')  
+      res.redirect('/build-cohort/v14/add-condition-group')  
     }
 })
 
 // Contextual routing - add-condition-group.html
 
-router.post("/cohort-builder/v14/add-condition-group-answer", function (req, res) {
+router.post("/build-cohort/v14/add-condition-group-answer", function (req, res) {
   // grab value from the data store
   let complete = req.session.data.complete
   // if the journey is complete send back to the 'check-your-cohort' page
   console.log(req.session.data)
   if (complete === 'true') {
-    res.redirect('/cohort-builder/v14/check-cohort')  
+    res.redirect('/build-cohort/v14/check-cohort')  
   } else {  
-    res.redirect('/cohort-builder/v14/check-condition-groups')  
+    res.redirect('/build-cohort/v14/check-condition-groups')  
   }
 })
 
 // Contextual routing - check-condition-group.html
 
-router.post("/cohort-builder/v14/check-condition-group-answer", function (req, res) {
+router.post("/build-cohort/v14/check-condition-group-answer", function (req, res) {
   // grab value from the data store
   let complete = req.session.data.complete
   // if the journey is complete send back to the 'check-your-cohort' page
   console.log(req.session.data)
   if (complete === 'true') {
-    res.redirect('/cohort-builder/v14/check-cohort')  
+    res.redirect('/build-cohort/v14/check-cohort')  
   } else {  
-    res.redirect('/cohort-builder/v14/check-cohort')  
+    res.redirect('/build-cohort/v14/check-cohort')  
   }
 })
 
 // Set journey as complete - check-cohort.html
 
-router.get("/cohort-builder/v14/check-cohort", function (req, res) {  
+router.get("/build-cohort/v14/check-cohort", function (req, res) {  
     // set data store variable
     console.log("BEFORE", req.session.data)
     req.session.data.complete = 'true'
     console.log("AFTER", req.session.data)
     // render the page
-    return res.render('cohort-builder/v14/check-cohort')
+    return res.render('build-cohort/v14/check-cohort')
 })
+
+// DevMode
 
 function devModeRoute(req, res, next) {
   if (!req.session.data['devMode']) {
